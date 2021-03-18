a5b364bb946a2351862e98b8b05490e3a079e574
Cameron <61031428+CameronWehrfritz@users.noreply.github.com>
2021-03-17 17:52:34 -0700

    Small changes
    i. updated some excel formatting, such as which columns to widen and by how much
    ii. updated some comments for clarity

--- Process_MS_Candidates_File.R
+++ Process_MS_Candidates_File.R
@@ -1,7 +1,7 @@
 #Written by Cameron Wehrfritz
 #Schilling Lab, Buck Institute for Research on Aging
 #Novato, California, USA
-#March 11, 2021
+#March 10, 2021
 
 # DESCRIPTION: This script processes Mass Spectrometry Candidates Files
 # i. cleans data
@@ -12,10 +12,11 @@
 
 ### Begin Script ###
 
+
 #------------------------------------------------------------------------------------
 #set working directory
-# setwd("/Volumes/GibsonLab/users/Cameron/2021_0310_E20_CRUK/R_workspace") # MAC
-setwd("//bigrock/GibsonLab/users/Cameron/2021_0310_E20_CRUK/R_workspace") # PC
+# setwd("/Volumes/GibsonLab/users/Cameron/2021_0310_E21_CRUK/R_workspace") # MAC
+setwd("//bigrock/GibsonLab/users/Cameron/2021_0310_E21_CRUK/R_workspace") # PC
 #------------------------------------------------------------------------------------
 
 
@@ -35,10 +36,11 @@ package.check <- lapply(packages, FUN = function(x) {
 #------------------------------------------------------------------------------------
 # LOAD DATA #
 
-df.input <- read.csv("//bigrock/GibsonLab/users/Cameron/2021_0310_E20_CRUK/Input/Candidates_PG20_E20_0305_2021_v2.tsv", sep="\t", stringsAsFactors = F) #PC
+# load data from MASTER repository
+df.input <- read.csv("//bigrock/GibsonLab/users/Cameron/2020_0701_CRUK_STORMing_Cancer_MASTER/Data_files/Esophagus/Candidates_PG25_E21_EN02TrueNormal_2021_0315_v3.tsv", sep="\t", stringsAsFactors = F) # PC
 
 # define file name for output file
-input.filename <- "Candidates_PG20_E20_0305_2021_v2.tsv"
+input.filename <- "Candidates_PG25_E21_EN02TrueNormal_2021_0315_v3.tsv" # manually add this
 #------------------------------------------------------------------------------------
 
 
@@ -52,7 +54,11 @@ df <- df.input %>%
          Percent.Change=X..Change,
          Number.of.Unique.Total.Peptides=X..Unique.Total.Peptides,
          Number.of.Unique.Total.EG.Id=X..Unique.Total.EG.Id) %>%
-  select(UniProtIds, Genes, ProteinGroups, ProteinNames, ProteinDescriptions, # move these variables to the beginning
+  select(UniProtIds, Genes, # move these variables to the beginning (we will widen these columns in the excel workbook)
+         ProteinGroups, ProteinNames, ProteinDescriptions, 
+         Comparison..group1.group2., 
+         AVG.Log2.Ratio, Qvalue, Absolute.AVG.Log2.Ratio,
+         GO.Biological.Process, GO.Molecular.Function, GO.Cellular.Component, 
          everything()) %>% # all other variables follow
   arrange(Qvalue) # arrange by Qvalue, ascending
 #------------------------------------------------------------------------------------
@@ -63,8 +69,9 @@ df <- df.input %>%
 # which comparisons do we want? 
 unique(df$Comparison..group1.group2.) # run this to print to screen comparisons which are present in the data
 # which comparisons need to be flipped?
-comparisons.to.flip <- c() # currently empty # add the comparisons you wish to flip and correct by hand
-                         
+comparisons.to.flip <- c("E_21_C / E_21_B") # add the comparisons you wish to flip and correct by hand
+# check that these comparisons are in the dataset (confirm no typos)
+comparisons.to.flip %in% unique(df$Comparison..group1.group2.) # should all be true
 
 # correction function
 # some comparisons are inverted, and need to be flipped and corrected 
@@ -102,9 +109,8 @@ flip.and.correct.fun <- function(df.x, comparisons){
   return(df.out)
 }
 
-
 # apply correction function to dataframe
-df.corrected <- flip.and.correct.fun(df, comparisons.to.flip) # correct these comparisons - vector created above by hand, at the beginning of this chunk
+df.corrected <- flip.and.correct.fun(df, comparisons.to.flip) # correct these comparisons
 
 # print comparison ratios to screen - are these the ones we want?
 unique(df.corrected$Comparison..group1.group2.)
@@ -127,7 +133,7 @@ df.inspect <- df.corrected %>%
                           AVG.Log2.Ratio, 
                           Absolute.AVG.Log2.Ratio, 
                           Ratio), 
-            by=c("UniProtIds", "Genes", "Absolute.AVG.Log2.Ratio"))
+            by=c("UniProtIds", "Genes", "Absolute.AVG.Log2.Ratio")) # also joining by Absolute.Log2.Ratio in order to get the correct match
 
 # rename corrected dataframe for simplicity
 df <- df.corrected
@@ -164,58 +170,58 @@ setColWidths(wb, sheet.name, cols=1:length(colnames(df)), widths = 10) # set all
 
 
 # Hide the following columns:
-# "AVG.Group.Quantity.Denominator"
-# "AVG.Group.Quantity.Numerator"
-# "X..Change" 
-# "Standard.Deviation"
-columns_to_hide <- c("AVG.Group.Quantity.Denominator", "AVG.Group.Quantity.Numerator", "Number.of.Ratios", "Percent.Change", "Standard.Deviation")
+columns_to_hide <- c("AVG.Group.Quantity.Denominator", "AVG.Group.Quantity.Numerator", 
+                     "Number.of.Ratios", "Percent.Change", "Standard.Deviation", 
+                     "Condition.Numerator", "Condition.Denominator", 
+                     "Degrees.Of.Freedom", "Standard.Error")
 column.hide.indeces <- which(colnames(df) %in% columns_to_hide)
 
-# hide columns:
+# hide columns
 setColWidths(wb, sheet.name, cols = column.hide.indeces, widths = 8.43, hidden = TRUE, ignoreMergedCells = FALSE)
 
 
 # WIDEN
 # Widen the following columns for better legibility:
 # "UniProtIds" # set to 15
+# "Genes" # set to 10
 # "ProteinGroups" # set to width of 18
 # "ProteinNames" # set to width of 20
 # "ProteinDescriptions" # set to width of 40
 # "Comparison..group1.group2." # set to width of 20
-# "Condition.Numerator" # set to width of 15
-# "Condition.Denominator" # set to width of 15
 # "AVG.Log2.Ratio" # set to width of 19
+# "Qvalue" # set to width of 12
 # "Absolute.AVG.Log2.Ratio" # set to width of 19
 # "GO.Biological.Process" # set to width of 55
 # "GO.Molecular.Function" # set to width of 55
 # "GO.Cellular.Component" # set to width of 55
-columns_to_widen <- c("UniProtIds", "ProteinGroups", "ProteinNames", "ProteinDescriptions", 
-                      "Comparison..group1.group2.", "Condition.Numerator", "Condition.Denominator", 
-                      "AVG.Log2.Ratio", "Absolute.AVG.Log2.Ratio",
+columns_to_widen <- c("UniProtIds", "Genes", 
+                      "ProteinGroups", "ProteinNames", "ProteinDescriptions", 
+                      "Comparison..group1.group2.", 
+                      "AVG.Log2.Ratio", "Qvalue", "Absolute.AVG.Log2.Ratio",
                       "GO.Biological.Process", "GO.Molecular.Function", "GO.Cellular.Component")
 
 column.widen.indeces <- which(colnames(df) %in% columns_to_widen)
 
 # set widths for each column above (the order must match)
-column.widths <- c(15, 18, 20, 40, 
-                   20, 15, 15, 
-                   19, 19, 
+column.widths <- c(15, 10, 
+                   18, 20, 40, 
+                   20,
+                   19, 12, 19, 
                    55, 55, 55)
 
 # assign names to column.widths vector - to keep things straight
 names(column.widths) <- columns_to_widen
 
-setColWidths(wb, sheet = 1, cols = column.widen.indeces, widths = column.widths) # column widths
+# print them vector out to make sure widths are correct
+print(column.widths)
 
+# set widths in workbook
+setColWidths(wb, sheet = sheet.name, cols = column.widen.indeces, widths = column.widths) # column widths
 
 
 # CENTER
-# Center text:
-# columns to center:
-# "AVG.Log2.Ratio"
-# "Absolute.AVG.Log2.Ratio"
-# "Ratio"
-columns_to_center <- c("AVG.Log2.Ratio", "Absolute.AVG.Log2.Ratio", "Ratio")
+# center the following columns:
+columns_to_center <- c("AVG.Log2.Ratio", "Qvalue", "Absolute.AVG.Log2.Ratio", "Ratio")
 column.center.indeces <- which(colnames(df) %in% columns_to_center)
 
 # align center:
@@ -238,10 +244,6 @@ addStyle(wb, sheet.name, style = align_center_style, rows = 1:dim(df)[1], cols =
 # Make Second Sheet
 # This sheet contains all proteins with at least 2 unique peptides
 
-# # first set sheet name
-# sheet.name <- paste("sheet", x+1)
-# x <- x+1 # increment x
-
 sheet.name <- "All proteins 2+ peptides"
 
 
@@ -262,7 +264,6 @@ setColWidths(wb, sheet.name, cols = column.hide.indeces, widths = 8.43, hidden =
 setColWidths(wb, sheet = sheet.name, cols = column.widen.indeces, widths = column.widths) # column widths
 
 # CENTER
-# add style
 addStyle(wb, sheet = sheet.name, style = align_center_style, rows = 1:dim(df)[1], cols = column.center.indeces, gridExpand = TRUE, stack = TRUE)
 
 # END SHEET
@@ -271,22 +272,20 @@ addStyle(wb, sheet = sheet.name, style = align_center_style, rows = 1:dim(df)[1]
 
 #-----------------------------------------------------------------------------------------------------
 # Make one separate sheet for each comparison
+# which contains the significantly altered proteins with at least 2 unique peptides
+
 for(i in unique(df$Comparison..group1.group2.)){
   print(i)
   # sheet name is the comparison 
-  # tweaked comparison name slightly for writing to excel file
-  sheet.name <- gsub("\\/", "vs", i)
-    
-  # # first set sheet name
-  # sheet.name <- paste("sheet", x+1)
-  # x <- x+1 # increment x
+  # change comparison name slightly for writing to excel file
+  sheet.name <- gsub("\\/", "vs", i) # replace the slash division with 'vs'
 
   # significantly altered proteins with at least 2 unique peptides for this specific comparison
   df.sig <- df %>%
+    filter(Comparison..group1.group2.==i) %>% # filter for specific comparison for this sheet
     filter(Qvalue<=0.05) %>%
     filter(Absolute.AVG.Log2.Ratio>0.58) %>%
-    filter(Number.of.Unique.Total.Peptides>1) %>% # must have at least 2 unique peptides per protein
-    filter(Comparison..group1.group2.==i) # i is the specific comparison for this sheet
+    filter(Number.of.Unique.Total.Peptides>1) # must have at least 2 unique peptides per protein
   
   addWorksheet(wb, sheet.name, gridLines = TRUE)
   writeData(wb, sheet.name, df.sig, rowNames = FALSE)
@@ -311,12 +310,12 @@ for(i in unique(df$Comparison..group1.group2.)){
 # RENAME THE SHEETS
 # optional
 
-# names(wb) <- c("All Proteins", "Significant Proteins")
+# names(wb) <- c("All proteins", "All proteins 2+ peptides", "sheet 3")
 #-----------------------------------------------------------------------------------------------------
 
 
 #-----------------------------------------------------------------------------------------------------
-# create name for processed file
+# create name for processed output file
 output.filename <- input.filename %>%
   strsplit(split=".tsv") %>%
   unlist() %>%
@@ -328,3 +327,4 @@ output.filename <- input.filename %>%
 # SAVE WORKBOOK
 saveWorkbook(wb, file = output.filename, overwrite = TRUE)
 #-----------------------------------------------------------------------------------------------------
+
